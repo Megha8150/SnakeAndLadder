c6daa3b011565d5a496559bc95775e2f560102e3
Megha8150 <meghananayak436@gmail.com>
2022-07-13 23:42:55 +0530

    Welcome message

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day4;
+SnakeAndLadder.game();
+
+
---
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+     class SnakeAndLadder
+    {
+        public static void game()
+        {
+            Console.WriteLine("Welcome to Snake and Ladder");
+        }
+    }
+}
